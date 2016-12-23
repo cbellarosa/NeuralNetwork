c6fb87633359ccb24dfe74b8b16c2c1f653170ad
Cedrick Bellarosa <cbellarosa@free.fr>
2016-12-23 14:39:09 +0100

    Add project files.

---
+++ com.bellarosa.ia.NeuronalNetwork/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.bellarosa.ia.NeuronalNetwork
+{
+    public class Class1
+    {
+    }
+}
